20a2e76f8fd2fddb433cce43123505b45aac0eaa
Jenno44 <73479432+Jenno44@users.noreply.github.com>
2021-06-10 23:52:54 +0200

    Update itemdisplay.php

--- itemdisplay.php
+++ itemdisplay.php
@@ -28,3 +28,25 @@ while($row = $result->fetch_assoc())
 
 $iid = $row["item_id"];
 $iname= $row["item_n"];
+$ipic = $row["item_img"];
+$icurrentp = $row["current_bid"];
+$iimg = "images/";
+$iimg = $iimg.$row["item_img"];
+
+$link = "item_details.php?item_id=";
+$item_d = $link.$iid;
+echo "<div class='item'>";
+echo "<div class='item_row'>item Id: $iid</div>";
+echo "<div class='item_row'>Item name: $iname</div>";
+echo "<img class='item_img' src='$iimg' alt='image'>";
+echo "<div class='item_row'>Current bid: $$icurrentp</div>";
+echo "<div class='item_row' ><a class='link' href='$item_d'>Item details</a></div>";
+echo "</div>";
+}
+$conn->close();
+}
+
+
+     ?>
+  </body>
+</html>
