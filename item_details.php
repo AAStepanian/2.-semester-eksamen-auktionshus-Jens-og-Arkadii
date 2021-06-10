8f1d32070459ddc95917ee2cbb0c2e02136cb50f
Jenno44 <73479432+Jenno44@users.noreply.github.com>
2021-06-10 23:54:25 +0200

    Update item_details.php

--- item_details.php
+++ item_details.php
@@ -34,3 +34,38 @@ $row = $result->fetch_assoc();
 $iid = $row["item_id"];
 $iname = $row["item_n"];
 $i_desc = $row["item_d"];
+$iiprice = $row["init_bid"];
+$end = $row["time_end"];
+$bid_num = $row["bid_num"];
+$icprice = $row["current_bid"];
+$iimg = "images/";
+$iimg = $iimg.$row["item_img"];
+$username = !isset($_SESSION["username"]);
+echo "<div class='item'>";
+echo "<div class='item_row'>Item id: $iid</div>";
+echo "<div class='item_row'>item name: $iname</div>";
+echo "<img class='item_img' src='$iimg' alt='image'>";
+echo "<div class='item_row'>Item description: $i_desc</div>";
+echo "<div class='item_row'>End time: $end</div>";
+echo "<div class='item_row'>Number of bids: $bid_num</div>";
+echo "<form action='bid.php' method='POST'>";
+echo "<input type='hidden' value='$iid' name='item_id'/>";
+echo "<input type='hidden' value'$username' name='username'>";
+echo "<select name='bid'>";
+for($i=0; $i < 220; $i++)
+{
+$j = $i*10;
+echo "<option value='$j'>$$j</option>";
+}
+echo "</select>";
+echo "<input type='submit' value='Bid'>";
+echo "</form>";
+echo "</div>";
+
+$conn->close();
+
+}
+
+     ?>
+  </body>
+</html>
