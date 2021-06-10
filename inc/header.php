866600cd4d93927cdf572982d1e1ad17a8466ae2
AAStepanian <73479461+AAStepanian@users.noreply.github.com>
2021-06-10 23:32:49 +0200

    Images, header, index, css

---
+++ inc/header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+      <link rel="stylesheet" href="css/nav.css">
+    <meta charset="utf-8">
+    <title>IKEA Recycling auctionhouse</title>
+    <div class="nav">
+    <a href="index.php">Home</a>
+  <a href="itemdisplay.php">Buy</a>
+  <a href="login.php">Log-in</a>
+  <a href="sell_item.php">Sell</a>
+  <a href="signup.php">Sign-up</a>
+  <a href="mybids.php">My bids</a>
+  <a href="myitems.php">My items</a>
+</div>
+  </head>
+  <body>
