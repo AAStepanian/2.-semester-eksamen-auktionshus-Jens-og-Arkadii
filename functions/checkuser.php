0a1707746bccaa1a6b69ad7e993235675059e153
Jenno44 <73479432+Jenno44@users.noreply.github.com>
2021-06-11 00:02:03 +0200

    Update checkuser.php

--- functions/checkuser.php
+++ functions/checkuser.php
@@ -30,3 +30,25 @@ header("Location: ..\signup.php?user=$value");
 //adds user if the user is not a dublicate
 else
 {
+  $statement = "INSERT INTO user(username, password) VALUES(?, ?)";
+  $stmt = $conn->prepare($statement);
+  $stmt->bind_param("ss", $username, $hashed);
+  $stmt->execute();
+
+  $value = "success";
+  header("Location: ..\signup.php?user=$value");
+  } /*verifies if there is a duplicate user further */
+
+  $conn->close();
+  }
+
+  else
+  {
+  header("Location: ..\signup.php");
+  } /*verifying so that the user is not directly accessing this page*/
+
+  ?>
+
+
+    </body>
+  </html>
