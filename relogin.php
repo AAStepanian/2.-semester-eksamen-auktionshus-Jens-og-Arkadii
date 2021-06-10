dabe7054c2f710572dee33914249c4e854e1041e
Jenno44 <73479432+Jenno44@users.noreply.github.com>
2021-06-10 23:43:59 +0200

    Update relogin.php

--- relogin.php
+++ relogin.php
@@ -14,3 +14,13 @@ echo "<br>";
 echo "<form action='functions\logincontrol.php' class='relogin_form' method='POST'";
     echo "<h4>Please relog</h4>";
 echo "<label for='username' class='label'> Username:</label>";
+echo "input class='text' type='text' name='username' placeholder='Username'";
+echo "<br>";
+echo "input class='password' type='password' name='password' pladeholder='Password'";
+echo "<br>";
+echo "<input class='submit' type='submit' value'Log in'/>";
+echo "</form>";
+
+     ?>
+  </body>
+</html>
