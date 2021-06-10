2254fb3490292bd48a1e2379685e4ffc9832fb7e
AAStepanian <73479461+AAStepanian@users.noreply.github.com>
2021-06-11 00:09:30 +0200

    Update login.php

--- login.php
+++ login.php
@@ -10,7 +10,7 @@
     include 'inc\header.php';
 echo "<img id='logo' src='images\logo.png'/>";
 echo "<br>";
-echo "<form action='functions\logincontrol.php' class='login_form' method='POST'>'";
+echo "<form action='functions\logincontrol.php' class='login_form' method='POST'>";
 echo "<h4> Please login</h4>";
 echo "<label for='username' class='label'>Username:</label>";
 echo "<input class='text' type='text' name='username' placeholder='Username'>";
